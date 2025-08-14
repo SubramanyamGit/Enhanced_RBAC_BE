e15a7c34311eb7e1edb5878748d296602e611431
SubramanyamViewplus <subramanyam@viewplus.ie>
2025-08-14 12:20:18 +0100

    ENV varaibles updated

--- config/db.js
+++ config/db.js
@@ -1,21 +1,27 @@
 const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Root@123",
-  database: "enhanced_rbac",
-   waitForConnections: true,
+  host: process.env.DB_URL,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: { minVersion: "TLSv1.2", rejectUnauthorized: true },
 });
 
 connection.connect;
 (err) => {
   if (err) {
-    return   console.log("Error:" + err.message);
+    console.log("ERRRO");
+
+    return console.log("Error:" + err.message);
   }
-    console.log("Connected to the MySQL server.");
+  console.log("Connected to the MySQL server.");
 };
 
 module.exports = connection;
--- models/set_password.model.js
+++ models/set_password.model.js
@@ -2,7 +2,7 @@ const db = require("../config/db");
 
 const updateUserPassword = async (user_id, hashedPassword) => {
   await db.query(
-    "UPDATE users SET password = ?, must_change_password = 0 WHERE user_id = ?",
+    "UPDATE users SET password = ?, must_change_password = FALSE WHERE user_id = ?",
     [hashedPassword, user_id]
   );
 };
