de316d04f2d431a950e260a67263ddb2e5f53e0d
SubramanyamViewplus <subramanyam@viewplus.ie>
2025-08-14 12:33:42 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.use(errorHandler);
 
 // Start Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "127.0.0.1", () => {
+app.listen(PORT, "0.0.0.0", () => {
    console.log(`  Server running on http://127.0.0.1:${PORT}`);
 });
